6ef055f138e3f896a9370d49f345eab9d04be03a
alen <alen@touzi.org>
2017-07-21 17:53:05 +0800

    fix painc on Option is None

--- src/youdao.rs
+++ src/youdao.rs
@@ -6,11 +6,13 @@ use self::scraper::{Html, Selector};
 extern crate serde_json;
 use self::serde_json::Value;
 
-fn parser(html:&String) -> Vec<String> {
+fn parser(html: &String) -> Vec<String> {
     let mut res = Vec::<String>::new();
     let fragment = Html::parse_fragment(html);
     let selector = Selector::parse("div#phrsListTab div.trans-container ul li").unwrap();
-    let selector_zh = Selector::parse("div#phrsListTab div.trans-container ul p.wordGroup span.contentTitle a").unwrap();
+    let selector_zh = Selector::parse("div#phrsListTab div.trans-container ul p.wordGroup \
+                                       span.contentTitle a")
+                          .unwrap();
     let selector_mo = Selector::parse("div#authDictTrans ul li span.wordGroup").unwrap();
 
     // web
@@ -24,21 +26,21 @@ fn parser(html:&String) -> Vec<String> {
         res.push(element.inner_html());
     }
 
-    /* // dict
-    for element in fragment.select(&selector_mo) {
-        res.push(element.inner_html());
-    }
-    */
+    // // dict
+    // for element in fragment.select(&selector_mo) {
+    // res.push(element.inner_html());
+    // }
+    //
 
     return res;
 }
 
-pub fn query(word:String) -> Vec<String> {
-    let mut w : String = String::new();
+pub fn query(word: String) -> Vec<String> {
+    let mut w: String = String::new();
     let ba = word.as_bytes();
     if ba[0] > 127 {
         for i in ba {
-            w = format!("{}%{:02X}",w, i);
+            w = format!("{}%{:02X}", w, i);
         }
     } else {
         w = word.clone();
@@ -50,91 +52,88 @@ pub fn query(word:String) -> Vec<String> {
 
     easy.url(&url_str).unwrap();
     {
-    let mut transfer = easy.transfer();
-    transfer.write_function(|data| {
-        // save respond
-        respond.extend_from_slice(data);
-        Ok(data.len())
-    }).unwrap();
-    transfer.perform().unwrap();
+        let mut transfer = easy.transfer();
+        transfer.write_function(|data| {
+                    // save respond
+                    respond.extend_from_slice(data);
+                    Ok(data.len())
+                })
+                .unwrap();
+        transfer.perform().unwrap();
     }
-    //println!("{}", easy.response_code().unwrap());
+    // println!("{}", easy.response_code().unwrap());
 
     let html = String::from_utf8(respond).unwrap();
-    return  parser(&html);
+    return parser(&html);
 }
 
 
-fn parser2(html:&String) -> Vec<String> {
+fn parser2(html: &String) -> Vec<String> {
     let mut res = Vec::<String>::new();
 
 
     let trans = serde_json::from_str::<Value>(&html[..]).unwrap();
     if trans["errorCode"].as_i64().unwrap() == 0 {
         // let mut config = STANDARD;
-        //println!("content:{}", String::from_utf8(decode(lrc["content"].as_str().unwrap()).unwrap()).unwrap());
-        // chinese
-		let base = trans["basic"].as_object().unwrap();
-		// let explains = base["explains"].as_array().unwrap()
-    	for element in base["explains"].as_array().unwrap() {
-        	res.push(element.as_str().unwrap().to_string());
-			println!("explains:{}", element);
-    	}
-    	//let base = trans["web"].as_object().unwrap();
-		match trans["web"].as_object() {
-			Option::Some(val) => {
-			for element in val["value"].as_array().unwrap() {
-        	res.push(element.as_str().unwrap().to_string());
-			println!("explains:{}", element);
-    	}
-			},
-			Option::None => println!("is null"),
-		}
-		// let explains = base["explains"].as_array().unwrap()
+        // println!("content:{}", String::from_utf8(decode(lrc["content"].as_str().unwrap()).unwrap()).unwrap());
+        match trans["basic"].as_object() {
+            Option::Some(base) => {
+                // let explains = base["explains"].as_array().unwrap()
+                for element in base["explains"].as_array().unwrap() {
+                    res.push(element.as_str().unwrap().to_string());
+                    println!("explains:{}", element);
+                }
+            }
+            Option::None => {}
+        }
 
+        match trans["web"].as_object() {
+            Option::Some(val) => {
+                for element in val["value"].as_array().unwrap() {
+                    res.push(element.as_str().unwrap().to_string());
+                    println!("explains:{}", element);
+                }
+            }
+            Option::None => println!("is null"),
+        }
     } else {
         println!("bad json object");
     }
 
-
-
-    /* // dict
-    for element in fragment.select(&selector_mo) {
-        res.push(element.inner_html());
-    }
-    */
-
     return res;
 }
 
-pub fn query2(word:String) -> Vec<String> {
-    let mut w : String = String::new();
+pub fn query2(word: String) -> Vec<String> {
+    let mut w: String = String::new();
     let ba = word.as_bytes();
     if ba[0] > 127 {
         for i in ba {
-            w = format!("{}%{:02X}",w, i);
+            w = format!("{}%{:02X}", w, i);
         }
     } else {
         w = word.clone();
     }
-    let url_str: String = format!("http://fanyi.youdao.com/openapi.do?keyfrom=f2ec-org&key=1787962561&type=data&doctype=json&version=1.1&q={}", w);
+    let url_str: String = format!("http://fanyi.youdao.com/openapi.\
+                                   do?keyfrom=f2ec-org&key=1787962561&type=data&doctype=json&version=1.\
+                                   1&q={}",
+                                  w);
 
     let mut easy = Easy::new();
     let mut respond = Vec::new();
 
     easy.url(&url_str).unwrap();
     {
-    let mut transfer = easy.transfer();
-    transfer.write_function(|data| {
-        // save respond
-        respond.extend_from_slice(data);
-        Ok(data.len())
-    }).unwrap();
-    transfer.perform().unwrap();
+        let mut transfer = easy.transfer();
+        transfer.write_function(|data| {
+                    // save respond
+                    respond.extend_from_slice(data);
+                    Ok(data.len())
+                })
+                .unwrap();
+        transfer.perform().unwrap();
     }
-    //println!("{}", easy.response_code().unwrap());
+    // println!("{}", easy.response_code().unwrap());
 
     let html = String::from_utf8(respond).unwrap();
-    return  parser2(&html);
+    return parser2(&html);
 }
-
